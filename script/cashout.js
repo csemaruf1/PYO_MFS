e4a45ff375557113afbbc503e42d382333e51adc
csemaruf1 <csemaruf200@gmail.com>
2026-03-13 17:27:43 +0800

    Update website design

--- script/cashout.js
+++ script/cashout.js
@@ -1,37 +1,73 @@
-document.getElementById("btn-cashout").addEventListener('click', function () {
-    const chshoutNumbarInput = document.getElementById('cashout-numbar');
-    const cashoutNumbar = chshoutNumbarInput.value;
-    console.log(cashoutNumbar);
-    if(cashoutNumbar.length != 11){
+document.getElementById('btn-cashout').addEventListener('click', function () {
+    const cashoutNambar = getValuFromInput('cashout-numbar');
+    // console.log(cashoutNambar);
+    if(cashoutNambar.length != 11){
         alert("Invalid Agent Numbar");
         return;
     }
 
-    const amountNumbarInput = document.getElementById("amount-numbar");
-    const amountNumbar = amountNumbarInput.value;
-    console.log(amountNumbar);
+    const cashoutAmount = getValuFromInput('amount-numbar');
 
-    const balanceElemant = document.getElementById("balance");
+    const balanceElemant = document.getElementById('balance');
     const balance = balanceElemant.innerText;
     console.log(balance);
-
-    const newbalance = Number(balance) - Number(amountNumbar);
-
-    if (newbalance < 0) {
-        alert("Invalid amount");
+    // calculat
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    // console.log(newBalance);
+    if(newBalance < 0){
+        alert("Invalid Amount!")
         return;
     }
 
-
-    const pinInput = document.getElementById("chshout-pin");
-    const pin = pinInput.value;
-    if (pin === '1234') {
-        alert("cashout successful");
-        console.log("NEW balance :", newbalance);
-        balanceElemant.innerText = newbalance;
+    //pin
+    const pin = getValuFromInput("chshout-pin");
+    if(pin === '1234'){
+        alert('cashout successful');
+        balanceElemant.innerText = newBalance;
     }
-    else {
-        alert("Invalid PIN");
+    else{
+        alert('Invalid PIN!');
         return;
     }
 })
+
+
+
+
+
+// document.getElementById('btn-cashout').addEventListener('click', function () {
+//     const cashoutInputNambar = document.getElementById('cashout-numbar');
+//     const cashoutNambar = cashoutInputNambar.value;
+//     console.log(cashoutNambar);
+//     if (cashoutNambar.length != 11) {
+//         alert("Invalid Agent Numbar!");
+//         return;
+//     }
+
+//     const ammountInputNambar = document.getElementById("amount-numbar");
+//     const cashoutAmount = ammountInputNambar.value;
+//     console.log(cashoutAmount);
+
+//     const balanceElemant = document.getElementById('balance');
+//     const balance = balanceElemant.innerText;
+//     console.log(balance);
+
+//     const newBalance = Number(balance) - Number(cashoutAmount);
+//     if (newBalance < 0) {
+//         alert("Invalid Amount!")
+//         return;
+//     }
+
+//     const pinInput = document.getElementById("chshout-pin");
+//     const pin = pinInput.value;
+//     if (pin == '1234') {
+//         alert('cashout successful');
+//         console.log("new balance :", newBalance);
+//         balanceElemant.innerText = newBalance;
+
+//     }
+//     else{
+//         alert("Invalid PIN!")
+//         return;
+//     }
+// })
---
+++ script/mashing.js
@@ -0,0 +1,9 @@
+// console.log("macshing added")
+
+// machine id input
+function getValuFromInput(id) {
+    const input = document.getElementById(id);
+    const valu = input.value;
+    console.log(valu);
+    return valu;
+}
